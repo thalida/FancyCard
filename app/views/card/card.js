88de16d6d0646186a5503c23a9e469f98fc2799d
Thalida Noel <thalida.c.noel@gmail.com>
2015-08-06 16:31:56 -0400

    Card: Updated terms with flipping card

--- app/views/card/card.js
+++ app/views/card/card.js
@@ -13,19 +13,19 @@ app.config([
 app.controller('CardCtrl', [
 	'$scope',
 	function($scope) {
-		$scope.flipCard = false;
+		$scope.isFrontShown = true;
 		$scope.runAnimation = true;
 
-		$scope.toggleFlip = function(){
-			$scope.flipCard = !$scope.flipCard;
-			$scope.stopAnimation();
+		$scope.flipCard = function(){
+			$scope.isFrontShown = !$scope.isFrontShown;
+			$scope.pauseAnimation();
 		};
 
-		$scope.stopAnimation = function(){
+		$scope.pauseAnimation = function(){
 			$scope.runAnimation = false;
 		};
 
-		$scope.startAnimation = function(){
+		$scope.playAnimation = function(){
 			$scope.runAnimation = true;
 		};
 	}
