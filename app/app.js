ef4d68f0506a269d9accd0d6be0464db0ebef2d2
Thalida Noel <thalida.c.noel@gmail.com>
2015-08-06 22:12:49 -0400

    Updated contrastcolor + cleaned up sass

--- app/app.js
+++ app/app.js
@@ -12,11 +12,12 @@ app.config(['$routeProvider', function($routeProvider) {
 
 app.run([function(){
 	// method taken from https://gist.github.com/960189
+	// 180.5
 	// 160.5
 	// 131.5
 	jQuery.Color.fn.contrastColor = function() {
 	    var r = this._rgba[0], g = this._rgba[1], b = this._rgba[2];
-	    return (((r*299)+(g*587)+(b*144))/1000) >= 180.5 ? 'black' : 'white';
+	    return (((r*299)+(g*587)+(b*144))/1000) >= 200 ? 'black' : 'white';
 	};
 
 	// shim layer with setTimeout fallback
