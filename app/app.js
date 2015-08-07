0ac4ebca047710ce0e3b7b21a148057b8d8b10c3
Thalida Noel <thalida.c.noel@gmail.com>
2015-08-06 22:05:26 -0400

    Card Tags: Added dynamic colors to the tags

--- app/app.js
+++ app/app.js
@@ -13,14 +13,11 @@ app.config(['$routeProvider', function($routeProvider) {
 app.run([function(){
 	// method taken from https://gist.github.com/960189
 	// 160.5
+	// 131.5
 	jQuery.Color.fn.contrastColor = function() {
 	    var r = this._rgba[0], g = this._rgba[1], b = this._rgba[2];
 	    return (((r*299)+(g*587)+(b*144))/1000) >= 180.5 ? 'black' : 'white';
 	};
-	// jQuery.Color.fn.contrastColor = function() {
-	//     var r = this._rgba[0], g = this._rgba[1], b = this._rgba[2];
-	//     return (((r*299)+(g*587)+(b*144))/1000) >= 131.5 ? 'black' : 'white';
-	// };
 
 	// shim layer with setTimeout fallback
 	window.requestAnimFrame = (function(){
--- app/views/card/card.controller.js
+++ app/views/card/card.controller.js
@@ -25,6 +25,38 @@ app.controller('CardCtrl', [
 			'assets/images/me_door.jpg',
 			'assets/images/me_hat.jpg'
 		]);
+		$scope.skills = [
+			{
+				weight: 'strong',
+				type: 'framework',
+				label: 'AngularJS'
+			},
+			{
+				weight: 'strong',
+				type: 'preprocessor',
+				label: 'CoffeScript'
+			},
+			{
+				weight: 'medium',
+				type: 'builder',
+				label: 'Grunt'
+			},
+			{
+				weight: 'light',
+				type: 'builder',
+				label: 'Gulp'
+			},
+			{
+				weight: 'light',
+				type: 'framework',
+				label: 'Backbone'
+			},
+			{
+				weight: 'light',
+				type: 'framework',
+				label: 'React'
+			}
+		];
 
 		$scope.flipCard = function(){
 			$scope.isFrontShown = !$scope.isFrontShown;
@@ -39,8 +71,29 @@ app.controller('CardCtrl', [
 			$scope.runAnimation = true;
 		};
 
-		$scope.$watchCollection('fancyTime', function(){
-			console.log( $scope.fancyTime );
-		});
+		$scope.getTagColor = function( tag ){
+			if( $scope.fancyTime === null ){
+				return '';
+			}
+			var currSaturation = $scope.fancyTime.color.saturation();
+			var newColor = $scope.fancyTime.color.saturation( currSaturation - 0.3 );
+
+			var rgba = newColor.rgba().join(', ');
+			var rgb = rgba.substring(0, rgba.length - 1);
+			var newAlpha;
+			var tagColor;
+
+			if( tag.weight === 'strong' ){
+				newAlpha = 1;
+			} else if( tag.weight === 'medium' ){
+				newAlpha = 0.5;
+			} else {
+				newAlpha = 0.2;
+			}
+
+			tagColor = 'rgba(' +  rgb + newAlpha + ')';
+
+			return tagColor;
+		};
 	}
 ]);
